0c4dd8d930e603fe7d386e19632d9ba768beca11
leoseg <leoseeger16@googlemail.com>
2022-05-13 16:27:21 +0200

    first commit added routing in app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NewChildForm from './NewChildForm'
+import * as React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+    render() {
+        return (
+            <Router>
+                <Routes>
+                    <Route path='/' exact={true} element={<NewChildForm/>}/>
+                </Routes>
+            </Router>
+        )
+    }
 }
 
 export default App;
